d4e1b5840ae20b4bf3c877357f5d6b9afa09117d
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-04-27 20:00:02 +0200

    parallel

--- eigenvalues.py
+++ eigenvalues.py
@@ -14,7 +14,7 @@ from image import Image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compute eigenvalues of an image')
-    parser.add_argument('files', metavar='F', nargs='+', help='the pgm image you want to compute the eigenvalues')
+    parser.add_argument('dir', help='the directory of the pgm images you want to compute the eigenvalues')
     parser.add_argument('--output', help='output file', required=True)
     parser.add_argument('--ncpus', help='number of cpus', type=int)
     parser.set_defaults(ncpus=1,)
@@ -24,20 +24,14 @@ if __name__ == "__main__":
     if os.path.isfile(args.output):
         output = pickle.load(open(args.output, "rb" ))
 
-    def parallel_compute_eigenvalues(path_image):
-        try:
-            im = Image(path_image)
+    def parallel_compute_eigenvalues(name):
+        if not name in output:
+            im = Image(args.dir+'/'+name)
             im.normalize()
-            eigenvalues = laplacian.compute_eigenvalues(im.image)
-            print(path_image)
-            return eigenvalues
-        except ValueError:
-            print('Warning: a problem occurs with ' + path_image)
+            output[name] = laplacian.compute_eigenvalues(im.image)
+            print(name + ' has been processed')
 
     pool = Pool(args.ncpus)
-    for path_image in args.files:
-        name = path_image.split('/')[-1].split('.')[0]
-        if not name in output:
-            output[name] = pool.apply_async(parallel_compute_eigenvalues, [path_image]).get()
+    pool.map(parallel_compute_eigenvalues,os.listdir(args.dir))
 
     pickle.dump(output, open(args.output, "wb"))
