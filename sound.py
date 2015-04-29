7a18705f2764fdca807aed3fdfb9317b33b0bada
Quentin Cormier <quentincormier@gmail.com>
2015-04-29 19:16:23 +0200

    sound

--- sound.py
+++ sound.py
@@ -6,6 +6,9 @@ from pyaudio import PyAudio
 import laplacian
 from image import Image
 import numpy as np
+import os.path
+import pickle
+
 
 def sine_tone(frequencies, amplitudes, duration, volume=1.0, sample_rate=22050):
     n_samples = int(sample_rate * duration)
@@ -35,24 +38,37 @@ def sine_tone(frequencies, amplitudes, duration, volume=1.0, sample_rate=22050):
     p.terminate()
 
 
-
-
+def compute_eigenvalues(img_file):
+    im = Image(img_file)
+    im.normalize()
+    return laplacian.compute_eigenvalues(im.image)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Play an image')
     parser.add_argument('file', help='the image')
+    parser.add_argument('--eigenvalues', help='the precomputed eigenvalues')
+    parser.set_defaults(eigenvalues=1)
     args = parser.parse_args()
-    im = Image(args.file)
-    im.normalize()
-    eigenvalues = np.array(laplacian.compute_eigenvalues(im.image), dtype=float)
+
+    if os.path.isfile(args.eigenvalues):
+        print(args.file.split('/')[-1])
+        eigenvalues_list = pickle.load(open(args.eigenvalues, "rb"))
+        if args.file.split('/')[-1] in eigenvalues_list:
+            eigenvalues = eigenvalues_list[args.file.split('/')[-1]]
+        else:
+            eigenvalues = compute_eigenvalues(args.file)
+    else:
+        eigenvalues = compute_eigenvalues(args.file)
+
+    eigenvalues = np.array(eigenvalues, dtype=float)
     frequencies = np.array(40 * np.sqrt(eigenvalues[0:3]), dtype=int)
     print(frequencies)
     sine_tone(
-    # see http://www.phy.mtu.edu/~suits/notefreqs.html
-    frequencies=frequencies.tolist(), # Hz, waves per second A4
-    amplitudes=np.ones(3).tolist(),
-    duration=2., # seconds to play sound
-    volume=.3, # 0..1 how loud it is
-    # see http://en.wikipedia.org/wiki/Bit_rate#Audio
-    sample_rate=22050 # number of samples per second
+        # see http://www.phy.mtu.edu/~suits/notefreqs.html
+        frequencies=frequencies.tolist(), # Hz, waves per second A4
+        amplitudes=np.ones(3).tolist(),
+        duration=2., # seconds to play sound
+        volume=.3, # 0..1 how loud it is
+        # see http://en.wikipedia.org/wiki/Bit_rate#Audio
+        sample_rate=22050 # number of samples per second
 )
