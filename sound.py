527e1fc693e84b2f667de934e1d1c27c1ee52918
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-05-01 12:30:42 +0200

    report

--- sound.py
+++ sound.py
@@ -4,7 +4,6 @@ from __future__ import division
 import math
 import argparse
 from pyaudio import PyAudio
-
 import eigenvalues
 from image import Image
 import numpy as np
@@ -14,7 +13,6 @@ import random
 import time
 
 # Using http://stackoverflow.com/questions/974071/python-library-for-playing-fixed-frequency-sound
-
 def sine_tone(frequencies, amplitudes, duration, volume=1.0, sample_rate=22050):
     n_samples = int(sample_rate * duration)
     restframes = n_samples % sample_rate
@@ -47,40 +45,12 @@ def produce_sound(eigenvalues): # produce a sound from eigenvalues
     frequencies = np.array(40 * np.sqrt(eigenvalues[0:3]), dtype=int)
     print(frequencies)
     sine_tone(
-        # see http://www.phy.mtu.edu/~suits/notefreqs.html
         frequencies=frequencies.tolist(), # Hz, waves per second A4
         amplitudes=np.ones(3).tolist(),
         duration=2., # seconds to play sound
         volume=.3, # 0..1 how loud it is
-        # see http://en.wikipedia.org/wiki/Bit_rate#Audio
         sample_rate=22050 # number of samples per second
     )
-
-def sound_game():
-    eigenvalues = pickle.load(open('eigenvalues/eigenvalues_arranged.db', "rb"))
-    dif = int(input('Enter the difficulty (2-10): ')) # number of sounds to heard
-    category = random.choice(list(eigenvalues.keys())) # a random category
-    sample = random.randint(0,len(eigenvalues[category])-1) # an object in this category
-    print("Listen this sample of " + category)
-    time.sleep(2)
-    produce_sound(eigenvalues[category][sample][1])
-    print("Now listen these " + str(dif) + " sounds:")
-    now = random.randint(0,dif-1) # time to listen an object of the same category than sample
-    for i in range(0,dif):
-      time.sleep(2)
-      print("Sound " + str(1+i))
-      if i == now:
-        produce_sound(eigenvalues[category][random.randint(0,len(eigenvalues[category])-1)][1])
-      else:
-        rand_cat = random.choice(list(eigenvalues.keys()))
-        while rand_cat == category:
-          rand_cat = random.choice(eigenvalues.keys())
-        produce_sound(eigenvalues[rand_cat][random.randint(0,len(eigenvalues[rand_cat])-1)][1])
-    answer = int(input("Which one of these sounds was %s?\n" % category))
-    if answer-1 == now:
-        print("Success")
-    else:
-        print("Fail, it was number " + str(now+1))
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Play an image')
---
+++ sound_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from sound import *
+
+# A small game using sound.py
+
+def sound_game():
+    eigenvalues = pickle.load(open('eigenvalues/eigenvalues_arranged.db', "rb"))
+    dif = int(input('Enter the difficulty (2-10): ')) # number of sounds to heard
+    category = random.choice(list(eigenvalues.keys())) # a random category
+    sample = random.randint(0,len(eigenvalues[category])-1) # a random object in this category
+    print("Listen this sample of " + category)
+    time.sleep(2)
+    produce_sound(eigenvalues[category][sample][1])
+    print("Now listen these " + str(dif) + " sounds:")
+    now = random.randint(0,dif-1) # time to listen an object of the same category than sample
+    for i in range(0,dif):
+      time.sleep(2)
+      print("Sound " + str(1+i))
+      if i == now: # a random object in the same category than sample
+        produce_sound(eigenvalues[category][random.randint(0,len(eigenvalues[category])-1)][1])
+      else:
+        rand_cat = random.choice(list(eigenvalues.keys()))
+        while rand_cat == category:
+          rand_cat = random.choice(eigenvalues.keys())
+        produce_sound(eigenvalues[rand_cat][random.randint(0,len(eigenvalues[rand_cat])-1)][1]) # a random object not in the category of sample
+    answer = int(input("Which one of these sounds was %s?\n" % category))
+    if answer-1 == now:
+        print("Success")
+    else:
+        print("Fail, it was number " + str(now+1))
+
+if __name__ == "__main__":
+    sound_game()
